31ab8333b7683afaa6286edb2a812602899c28f4
Heddie <42524625+130706472@users.noreply.github.com>
2021-10-07 00:40:33 -0700

    add bunch of functions

--- Lab1_analysis.m
+++ Lab1_analysis.m
@@ -1,7 +1,7 @@
 clc;clear;close all;
 
-% DATA = readtable('SSWT_DATA_2021.xlsx');
 DATA = load('DATA.mat').DATA;
+global t P_pitot P_plen P_tank P_stat gamma
 
 t = table2array(DATA(:,1));
 
@@ -17,7 +17,15 @@ P_tank = table2array(DATA(:,4));
 % Static pressure [psig] -> at the pitot tube
 P_stat = table2array(DATA(:,5));
 
+gamma = 1.4;
+
+% problem1()
+% problem4()
+
+
 %% Problem 1
+function [] = problem1()
+global t P_pitot P_plen P_tank P_stat
 figure(1)
 plot(t,P_pitot,'DisplayName', 'Pitot Tube Pressure')
 hold on
@@ -28,9 +36,11 @@ grid on
 legend()
 xlabel('Time [s]')
 ylabel('Pressure [psig]')
+end
 
 %% Problem 2
-
+function [] = problem2()
+global gamma
 % Throat width [inch]
 width_th = 1.02;
 % Throat area [in^2] -> critical area
@@ -40,23 +50,75 @@ A_throat = width_th.*4.8;
 A_test = 4.8^2; 
 
 % Test Section Mach number
-M_ts = critAinv(A_test/A_throat, 1.4);
+M_ts = critAinv(A_test/A_throat, gamma);
 M_ts = M_ts(2);
 
 % To do: find how accurate is the result.
+end
 
 %% Problem 3
+function [] = problem3()
+global t P_pitot P_plen gamma
 
+    P02oP01 = (P_pitot + 14.7)./(P_plen + 14.7);
+    M1_shock = [];
+    % M2 ---> Mach number after shock, i.e. on the pitot tube
+    M2_shock = [];
+    % Calculate M2 in shock relations
+    for i = 1:length(P02oP01)
+        M1_shock = [M1_shock; stagPres(P02oP01(i), gamma)];
+        M2_shock = [M2_shock; mach_change(M1_shock(i), gamma)];
+        fprintf(1,['t = %2.3f \t M1 = %1.4f \t M2 = %1.4f\n'],t(i),M1_shock(i),M2_shock(i))
+    end
+end
 
-
-
+%% Problem 4
+function [] = problem4()
+global t P_pitot P_stat gamma
+    P02oP2 = (P_pitot + 14.7)./(P_stat + 14.7);
+    M2_isen = [];
+    M1_shock = [];
+    for i = 1:length(P02oP2)
+        M2_isen = [M2_isen; isentroPress(P02oP2(i), gamma)];
+        M1_shock = [M1_shock; mach_change_inverse(M2_isen(i),gamma)];
+        fprintf(1,['t = %2.3f \t M1 = %1.4f \t M2 = %1.4f\n'],t(i),M1_shock(i),M2_isen(i))
+    end
+end
 
 %% Functions (Gas Dynamic)
 function [Mach] = critAinv(AoAst,gamma)
-%   AoA: A/A_star, ratio of area and critical area
+%   Given ration of A/A_critical, ratio of area and critical area
     syms M 
     eqn = AoAst == (1/M)*(((2/(gamma+1))*(1+((gamma-1)/2)*(M^2)))^((gamma+1)/(2*(gamma-1))));
     Mach = real(double(solve(eqn,M)));
+end
+
+function [M1] = stagPres(P02oP01, gamma)
+%   Given P_stag2/P_stag1, solve for upstream mach number
+    syms M
+    eqn = P02oP01 == (((gamma+1).*(M.^2))./((gamma-1).*(M.^2) + 2))...
+        .^(gamma/(gamma-1)).*((gamma+1)./((2.*gamma.*M^2) - (gamma-1)))...
+        .^(1/(gamma-1));
+    M1 = abs(max(real(double(vpasolve(eqn, M)))));
+end
+
+function [M2] = mach_change(M1,gamma)
+%   Normal shock realtion, solve downstream Mach by given upstream
+    M2 = sqrt(((gamma-1).*(M1.^2) + 2)./(2.*gamma.*(M1^2) - (gamma - 1)));
+end
+
+function [M1] = mach_change_inverse(M2,gamma)
+%   Normal Shock Relation, Solve upstream Mach by given downstream Mach
+    syms M
+    eqn = M2.^2 == ((gamma-1).*(M.^2) + 2)./(2.*gamma.*(M^2) - (gamma - 1));
+    M1 = abs(min(real(double(vpasolve(eqn, M)))));
+end
+
+function [M2] = isentroPress(P02oP2, gamma)
+%   Given P_stag/P_static, solve for flow mach number
+    syms M
+    eqn = P02oP2 == ((1 + ((gamma - 1)/2).*(M^2)).^(gamma./(gamma - 1)));
+    M2 = abs(max(real(double(solve(eqn, M)))));
 end
 
 
